c7b226cfe93d57d6bd9d439c0073078b60f9ba79
giovannifranzese94 <giovanni.franzese94@gmail.com>
2023-10-19 11:36:20 +0200

    added the possibility to set the rqt initial parameters to a default vlaue written in a config file. also added a visualization of the mass during the estimation to allow to debugging later on.

--- franka_smart_cartesian_impedance_control/python/mass_estimator.py
+++ franka_smart_cartesian_impedance_control/python/mass_estimator.py
@@ -5,17 +5,45 @@ from panda_ros import Panda
 from panda_ros.pose_transform_functions import array_quat_2_pose, pose_2_transformation, orientation_2_quaternion, list_2_quaternion
 from geometry_msgs.msg import PoseStamped
 from quaternion_algebra.algebra import quaternion_divide, to_euler_angles, from_euler_angles, quaternion_product
+from visualization_msgs.msg import Marker
+from geometry_msgs.msg import Vector3
 class MassEstimator(Panda):
     def __init__(self):
         rospy.init_node("mass_estimator_node")
         super(MassEstimator, self).__init__()
         self.set_mass = dynamic_reconfigure.client.Client('/dynamic_reconfigure_mass_param_node', config_callback=None)
         self.goal_sub = rospy.Subscriber('/equilibrium_pose', PoseStamped, self.ee_pos_goal_callback)
+        self.marker_pub = rospy.Publisher('camera_mass', Marker, queue_size=10)
         rospy.sleep(1)    
         self.estimated_mass=0
         self.estimated_offset_x=0
         self.estimated_offset_y=0
         self.estimated_offset_z=0
+    def visualize_mass(self):
+        while not(self.end):
+            # Create a marker message
+            marker = Marker()
+            marker.header.frame_id = "panda_EE"
+            marker.type = Marker.SPHERE
+            marker.action = Marker.ADD
+            marker.scale = Vector3(self.estimated_mass * 1000, self.estimated_mass * 1000, self.estimated_mass* 1000)  # Dimensions of the ellipsoid
+            marker.color.r = 1.0
+            marker.color.g = 0.0
+            marker.color.b = 0.0
+            marker.color.a = 0.8
+
+            # Set the position and orientation of the ellipsoid
+            marker.pose.position.x = self.estimatd_offset_x
+            marker.pose.position.y = self.estimatd_offset_y
+            marker.pose.position.z = self.estimatd_offset_z
+            marker.pose.orientation.x = 0.0
+            marker.pose.orientation.y = 0.0
+            marker.pose.orientation.z = 0.0
+            marker.pose.orientation.w = 1.0
+
+
+            # Publish the marker
+            self.marker_pub.publish(marker)
 
     def ee_pos_goal_callback(self, curr_conf):
         self.curr_pos_goal = np.array([curr_conf.pose.position.x, curr_conf.pose.position.y, curr_conf.pose.position.z])
@@ -35,7 +63,7 @@ class MassEstimator(Panda):
         curr_tranformation= pose_2_transformation(curr_pose.pose)
         orientation_matrix_transpose= np.transpose(curr_tranformation[0:2,:])
         goal_quat = list_2_quaternion(self.curr_ori_goal)
-        quat_diff= quaternion_divide(goal_quat, curr_quat ) # curr -gaol
+        quat_diff= quaternion_divide(goal_quat, curr_quat ) # goal-curr
         euler= to_euler_angles(quat_diff)
 
         gradient= orientation_matrix_transpose @ np.array([euler[1], - euler[0]])
@@ -51,7 +79,7 @@ class MassEstimator(Panda):
         curr_tranformation= pose_2_transformation(curr_pose.pose)
         orientation_matrix_transpose= np.transpose(curr_tranformation[0:2,:])
         goal_quat = list_2_quaternion(self.curr_ori_goal)
-        quat_diff= quaternion_divide(goal_quat, curr_quat ) # curr -gaol
+        quat_diff= quaternion_divide(goal_quat, curr_quat ) # goal-curr
         euler= to_euler_angles(quat_diff)
 
         gradient= orientation_matrix_transpose @ np.array([euler[1], - euler[0]])
@@ -59,8 +87,8 @@ class MassEstimator(Panda):
         # self.estimated_offset_y=self.estimated_offset_y-np.sign(gradient[1])
         self.estimated_offset_z=self.estimated_offset_z- np.floor(gradient[2]* self.estimated_mass *10)
         # self.set_mass.update_configuration({"offset_x": self.estimated_offset_x})
-        # self.set_mass.update_configuration({"offset_y": self.estimated_offset_y})
-        self.set_mass.update_configuration({"offset_z": self.estimated_offset_z})
+        # self.set_mass.update_configuration({"offset_y": self.estimeated_offset_y})
+        self.set_mass.update_configuration({"offset_z": self.estimatd_offset_z})
 
 
 
